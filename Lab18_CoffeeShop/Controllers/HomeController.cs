62b2c9fba57ab144dbbc0fa4507bb07e3ec0f84f
zeL <3DenzeL@protonmail.com>
2019-08-18 21:46:48 -0400

    first commit for Lab #18

---
+++ Lab18_CoffeeShop/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Lab18_CoffeeShop.Models;
+
+namespace Lab18_CoffeeShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View(new RegisterUser());
+        }
+
+        [HttpPost]    
+        public IActionResult Register(RegisterUser newUser)
+        {
+            return RedirectToAction("ShowRegistration", newUser);
+        }
+        
+        public IActionResult ShowRegistration(RegisterUser user)
+        {
+            return View(user);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Lab18_CoffeeShop/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lab18_CoffeeShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
