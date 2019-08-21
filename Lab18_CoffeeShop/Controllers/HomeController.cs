82ed540d905e0c84d89df574b0261aacf637db96
zeL <3DenzeL@protonmail.com>
2019-08-21 08:24:58 -0400

    first commit for Lab #20

--- Lab18_CoffeeShop/Controllers/HomeController.cs
+++ Lab18_CoffeeShop/Controllers/HomeController.cs
@@ -5,11 +5,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Lab18_CoffeeShop.Models;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
 
 namespace Lab18_CoffeeShop.Controllers
 {
     public class HomeController : Controller
     {
+        List<RegisterUser> userList = new List<RegisterUser>();
         public IActionResult Index()
         {
             return View();
@@ -18,6 +21,19 @@ namespace Lab18_CoffeeShop.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public IActionResult AddUserToList(RegisterUser user)
+        {
+            string userJson = HttpContext.Session.GetString("RegisterUserSession");
+            if(userJson != null)
+            {
+                userList = JsonConvert.DeserializeObject<List<RegisterUser>>(userJson);
+            }
+
+            userList.Add(user);
+            HttpContext.Session.SetString("RegisterUserSession", JsonConvert.SerializeObject(userList));
+            return View(userList);
         }
 
         [HttpGet]
@@ -29,8 +45,16 @@ namespace Lab18_CoffeeShop.Controllers
         [HttpPost]    
         public IActionResult Register(RegisterUser newUser)
         {
+
             if (ModelState.IsValid)
             {
+                string userJson = HttpContext.Session.GetString("RegisterUserSession");
+                if (userJson != null)
+                {
+                    userList = JsonConvert.DeserializeObject<List<RegisterUser>>(userJson);
+                }
+                userList.Add(newUser);
+                HttpContext.Session.SetString("RegisterUserSession",JsonConvert.SerializeObject(userList));
                 return RedirectToAction("ShowRegistration", newUser);
             }
             else
--- Lab18_CoffeeShop/Startup.cs
+++ Lab18_CoffeeShop/Startup.cs
@@ -31,7 +31,7 @@ namespace Lab18_CoffeeShop
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-
+            services.AddSession();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
@@ -52,6 +52,7 @@ namespace Lab18_CoffeeShop
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
+            app.UseSession();
 
             app.UseMvc(routes =>
             {
